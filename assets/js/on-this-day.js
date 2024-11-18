ade0a40df75888d3e798d8f2ca94ace6204399eb
Narendra Kashikar <narendra3012@gmail.com>
2024-11-18 19:49:59 +0530

    Add today-in-history app

---
+++ assets/js/on-this-day.js
@@ -0,0 +1,32 @@
+let today = new Date();
+const date = today.getDate();
+const month = today.getMonth()+1;
+const monthList = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+document.getElementById('daySpan').innerHTML = date;
+document.getElementById('monthSpan').innerHTML = monthList[month-1];
+
+
+
+async function fetchHistory(){
+    try{
+        const response = await fetch(`https://byabbe.se/on-this-day/${month}/${date}/events.json`);
+        if(!response.ok){
+            throw new Error(`Error Occures! Response error code: ${response.status}`);
+        } else{
+            const data = await response.json();
+            const tbody = document.getElementById('tableBody');
+            const eventsList = data['events'];
+            for (i=0;i<eventsList.length;i++){
+                var newRow = document.createElement('tr');
+                let eventYear = eventsList[i]['year'];
+                let eventDescr = eventsList[i]['description'];
+                newRow.innerHTML = `<td>${eventYear}</td><td>${eventDescr}</td>`;
+                tbody.append(newRow);
+            }
+
+        }
+} catch(error){
+    console.error(error);
+}
+}
+fetchHistory();
